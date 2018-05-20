4bd5cf868ed20b25121f196474445113dd05fac1
Leah Sandacz <sandas@outlook.com>
2018-05-19 22:32:55 -0700

    Baisc routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hi
+        <Router>
+          <Route exact path="/" component={Home}/>
+        </Router>
       </div>
     );
   }
